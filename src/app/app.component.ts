016da0fca141dd8c0c6be38d3cbddec953b4fd0b
tknott95 <tknott95@hotmail.com>
2016-10-15 23:41:32 -0600

    Full CRUD functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -91,6 +91,47 @@ export class AppComponent implements OnInit {
         this.changeState('default');
       }
 
+      showEdit(business){
+        this.changeState('edit', business.$key);
+        this.activeCompany = business.company;
+        this.activeCategory = business.category;
+        this.activeYearsInBusiness = business.years_in_business;
+        this.activeDescription = business.description;
+        this.activePhone = business.phone;
+        this.activeEmail = business.email;
+        this.activeStreetAddress = business.street_address;
+        this.activeCity = business.city;
+        this.activeState = business.state;
+        this.activeZipcode = business.zipcode;
+      }
+
+
+
+      updateBusiness(){
+          var updBusiness = {
+          company: this.activeCompany,
+          category: this.activeCategory,
+          years_in_business: this.activeYearsInBusiness,
+          description: this.activeDescription,
+          phone: this.activePhone,
+          email: this.activeEmail,
+          street_address: this.activeStreetAddress,
+          city: this.activeCity,
+          state: this.activeState,
+          zipcode: this.activeZipcode
+          }
+
+          this._firebaseService.updateBusiness(this.activeKey, updBusiness);
+
+          this.changeState('default');
+      }
+
+
+      deleteBusiness(key){
+        this._firebaseService.deleteBusiness(key);
+
+        this.changeState('default');
+      }
 
 
 
