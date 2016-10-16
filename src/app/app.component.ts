d74d5d822a70bbc8460ad92b57c2593321e6d1ce
tknott95 <tknott95@hotmail.com>
2016-10-15 19:22:10 -0600

    Businesses and Category services - c.log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { FirebaseService } from './services/firebase.service';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [FirebaseService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     items: FirebaseListObservable<any[]>;
 
-    constructor(af: AngularFire) {
-      this.items = af.database.list('items');
-  }
+    constructor(private _firebaseService:FirebaseService) {
+       
+    }
+
+    ngOnInit(){
+
+      this._firebaseService.getBusinesses().subscribe(businesses => {
+        console.log(businesses);
+      });
+
+      this._firebaseService.getCategory().subscribe(categories => {
+        console.log(categories);
+      });
+
+    }
+
+
 }
